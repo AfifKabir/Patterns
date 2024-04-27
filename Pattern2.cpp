803d4b443f9426aaf88bb8d0ddb83af0456a7a3f
Afif <afifkabir10@gmail.com>
2024-04-27 13:59:52 +0600

    U

--- Pattern2.cpp
+++ Pattern2.cpp
@@ -1,21 +1,73 @@
 /* 
-     *
-    **
-   ***
-   .....
+***
+**
+*
+.....
 */
 #include <stdio.h>
 int main() {
-    int n, col,space;
-    printf("Number of row:");
+    int n;
+    printf("N:");
     scanf("%d", &n);
-    for (int row = 1;row <=n;row++) {
-        for(int space=n-1;space>0;space--){
-            printf(" ");
-        }
-        for (int col = 1; col <= row; col++) {
+    for (int row = n;row>0;row--) {
+        for(int col=1;col<=row;col++){
             printf("*");
         }
         printf("\n");
     }
+/* 
+333
+22
+1
+*/
+for (int row = n; row > 0; row--) {
+        for (int col = 1; col <= row; col++) {
+            printf("%d",row);
+        }
+        printf("\n");
+    }
+/*
+123
+12
+1
+*/
+for (int row = n; row > 0; row--) {
+        for (int col = 1; col <= row; col++) {
+            printf("%d",col);
+        }
+        printf("\n");
+    }
+/*
+ABC
+AB
+A
+*/
+for (int row = n; row > 0; row--) {
+    for (int col = 1; col <= row; col++) {
+        printf("%c",col+64);
+    }
+    printf("\n");
+}
+/*
+CCC
+BB
+A
+*/
+for (int row = n; row > 0; row--) {
+    for (int col = 1; col <= row; col++) {
+        printf("%c",row+64);
+    }
+    printf("\n");
+ }
+/*
+###
+##
+#
+*/
+ for (int row = n; row > 0; row--) {
+     for (int col = 1; col <= row; col++) {
+         printf("#");
+     }
+     printf("\n");
+ }
 }
